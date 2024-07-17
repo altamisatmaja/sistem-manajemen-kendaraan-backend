6cb5cc3a87847faf5d8985c7d8d2799d8ebb0b41
altamisatmaja <altamisfattah16@gmail.com>
2024-07-17 23:37:20 +0700

    fix: sidebar for mobile

--- resources/views/components/sidebar-link.blade.php
+++ resources/views/components/sidebar-link.blade.php
@@ -1,11 +1,11 @@
 <ul class="space-y-2">
     <li
         class="pl-3 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-[linear-gradient(135deg,var(--tw-gradient-stops))] hover:bg-orange-400 hover:text-white duration-200">
-        <a href="{{ route('dashboard') }}" class="block text-gray-800  truncate transition">
+        <a href="{{ route('dashboard') }}" class="block text-gray-800 hover:text-white truncate transition">
             <div class="flex items-center hover:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px"
-                    fill="currentColor">
-                    <path fill-rule="evenodd" clip-rule="evenodd" class=""
+                    fill="currentColor" class="hover:text-white">
+                    <path fill-rule="evenodd" clip-rule="evenodd" class="hover:text-white"
                         d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600ZM200-600h160v-160H200v160Zm400 0h160v-160H600v160Zm0 400h160v-160H600v160Zm-400 0h160v-160H200v160Zm400-400Zm0 240Zm-240 0Zm0-240Z" />
                 </svg>
                 <span class="text-lg font-medium ml-4">Dashboard</span>
